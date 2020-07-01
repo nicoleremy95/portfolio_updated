0facdf37045db45aa6a675e62ad20aaf4c4f9010
Nicole Remy <nicoleremy95@gmail.com>
2020-06-30 21:21:49 -0700

    created an array with objects with the values of my homeworks, wrote a for each function for portfolios

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,6 @@ function aboutMe(){
     
     var aboutMeHeader = $("<h1>")
     aboutMeHeader.text("about me")
-    console.log(aboutMeHeader)
 
     var aboutMeContent = $("<h2>")
     aboutMeContent.text("Welcome to the website that tells you all about me. My name is Nicole, I am a Washingtonian, and I love music. I graduated from the University of Southern California in 2018 with a degree in GeoDesign. From there, I worked as an Architecture Associate for two years while dancing professionally in the NFL. Currently, I am studying to become a web developer. During my free time, you can find me enjoying long walks, reading, or drinking coffee while watching the sunrise. I am also an avid Netflix binger; my most recent of which is 'Riverdale'.")
@@ -18,8 +17,51 @@ function aboutMe(){
     
 }
 function portfolio(){
+    var portfolioSection = $("<div>")
+
+    var portfolioHeader = $("<h1>")
+    portfolioHeader.text("portfolio")
    
-    
+    var portfolioContent = [
+        {
+            "name": "hw1",
+            "info": "info",
+            "image": "assets/images/rsz_1screen_shot_2020-06-09_at_25837_pm.png",
+            "website": "https://github.com/nicoleremy95"
+        },
+        {
+            "name": "hw2",
+            "info": "info",
+            "image": "assets/images/rsz_1screen_shot_2020-06-09_at_25837_pm.png",
+            "website": "https://github.com/nicoleremy95"
+        }
+        
+
+    ];
+    console.log(portfolioContent)
+
+    portfolioContent.forEach(function(item){
+        var cardContent = $("<div class= 'card-content'>")
+        var cardAction = $("<div class = 'card-action'>")
+        var cardStacked = $("<div class = 'card-stacked'>")
+        var cardImage = $("<div class = 'card-image'>")
+        var cardHorizontal = $("<div class = 'card horizontal'>")
+
+        objName = portfolioContent[item].name
+        console.log(portfolioContent[item].name)
+        objInfo = portfolioContent[item].info
+        objImage = portfolioContent[item].image
+        objWebsite = portfolioContent[item].website
+
+        cardAction.append(objName)
+        cardContent.append(objInfo)
+        cardStacked.append(cardContent, cardAction)
+        cardImage.append(objImage)
+
+        cardHorizontal.append(cardImage, cardStacked)
+        portfolioSection.append(cardHorizontal)
+    })
+    content.append(portfolioHeader, portfolioSection)
 }
 function contactMe(){
   
