18d365b1c80b2b5f3c4b8988b405853259e550db
Nicole Remy <nicoleremy95@gmail.com>
2020-06-30 20:24:19 -0700

    created functions for all links in navbar, all functions working, aboutme function has content not styled yet

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,46 @@
-var pageHeader = $(".page-header")
-var contentSection = $(".content-section")
+var content = $(".content")
+
+
+function aboutMe(){
+    var aboutMeSection = $("<div>")
+    
+    var aboutMeHeader = $("<h1>")
+    aboutMeHeader.text("about me")
+    console.log(aboutMeHeader)
+
+    var aboutMeContent = $("<h2>")
+    aboutMeContent.text("Welcome to the website that tells you all about me. My name is Nicole, I am a Washingtonian, and I love music. I graduated from the University of Southern California in 2018 with a degree in GeoDesign. From there, I worked as an Architecture Associate for two years while dancing professionally in the NFL. Currently, I am studying to become a web developer. During my free time, you can find me enjoying long walks, reading, or drinking coffee while watching the sunrise. I am also an avid Netflix binger; my most recent of which is 'Riverdale'.")
+    console.log(aboutMeContent)
+    
+    aboutMeSection.append(aboutMeHeader, aboutMeContent)
+    content.html(aboutMeSection)
+    console.log(content)
+    
+}
+function portfolio(){
+   
+    
+}
+function contactMe(){
+  
+    
+}
+
+
+$(".about-me").click(function(event){
+    event.preventDefault();
+    aboutMe();
+    console.log("you clicked about me")
+})
+
+$(".portfolio").click(function(event){
+    event.preventDefault();
+    portfolio();
+    console.log("you clicked portfolio")
+})
+
+$(".contact-me").click(function(event){
+    event.preventDefault();
+    contactMe();
+    console.log("you clicked contact me")
+})
